52972cc480ac99db54240271c098da7ff47d361e
Darshik Hirapara <darshikhirapara@gmail.com>
2021-12-12 13:22:19 +0530

    added new changes

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+require('dotenv').config();
+const dbConfig = require("./src/config/db.config.js");
+const mongoose = require("mongoose");
+const bot = require("./src/bot");
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 7000;
+
+app.use(cors());
+app.use(helmet());
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/src/views/index.html"));
+});
+
+const connectWithDB = async () => {
+    await mongoose
+        .connect(dbConfig.url, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+        })
+        .then((connection) => {
+            console.log(`Connected with Database: ${connection.connection.name}`);
+        })
+        .catch((err) => {
+            console.error(err);
+            process.exit(0);
+        });
+};
+
+connectWithDB().then(() => {
+    bot.start();
+    app.listen(PORT, () => {
+        console.log(`Connected on port : ${PORT}`)
+    })
+})
--- src/bot.js
+++ src/bot.js
@@ -1,7 +1,3 @@
-require('dotenv').config();
-const dbConfig = require("./config/db.config.js");
-const mongoose = require("mongoose");
-
 const { CreateBroadCast, RefreshToken } = require('./routes')
 const User = require('./models/UserModel');
 const { parseString, toCode, toInlineCode } = require('./utils')
@@ -12,76 +8,64 @@ const { Client, Intents } = require('discord.js')
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const PREFIX = '$';
 
-client.on("ready", () => {
-    console.log(`${client.user.tag} has logged in.`);
-});
-
-client.on("messageCreate", async (message) => {
-    if (message.author.bot) return;
-    if (message.content.startsWith(PREFIX)) {
-        const [CMD_NAME, ...args] = message.content.trim()
-            .substring(PREFIX.length).replace(/[ ]+/g, " ").split(" ");
-        console.log(CMD_NAME);
-        console.log(args);
-        if (CMD_NAME === 'create') {
-            const userData = await User.findOne({ username: message.author.tag });
-            console.log(userData.expiry);
-            console.log(Date.now());
-            if (userData.expiry <= Date.now()) {
-                const tokenData = await RefreshToken(userData.refresh_token);
-                console.log(tokenData);
-                userData.expiry = (Number(tokenData.expires_in) * 1000) + Date.now() - 5;
-                userData.access_token = tokenData.access_token;
-                userData.id_token = tokenData.id_token;
-                await userData.save();
-            }
-            // console.log(userData);
-            if (!userData.title || !userData.description) {
-                message.reply(`title or description not available. Please set it by ${toInlineCode('$set \{title:<title>, description:<description>\}')}`)
-            }
-            const newTitle = parseString(userData.title, 0, Date.now());
-            const res = await CreateBroadCast({ title: newTitle, description: userData.description, access_token: userData.access_token });
-            // console.log(res);
-            message.reply(`msg \`\`\`${JSON.stringify(res)}\`\`\``);
-        } else if (CMD_NAME === 'set') {
-            try {
-                let userData = await User.findOne({ username: message.author.tag });
-                let newData = JSON.parse(args.join(" "));
-                console.log(args.join(" "));
-                const keys = Object.keys(newData);
-                console.log(keys);
-                for (let i of keys) {
-                    userData[i] = newData[i];
-                }
-                await userData.save();
-                message.reply("Data Saved Successfully!!!")
-            } catch (e) {
-                console.log(e);
-                message.reply("ERROR: " + e)
-            }
-        } else if (CMD_NAME === 'setimage') {
-            let userData = await User.findOne({ username: message.author.tag });
-            userData.imageURL = message.attachments.first().url;
-            await userData.save();
-            message.reply("Image uploaded Successfully!!!");
-        }
-    }
-});
-
-const connectWithDB = async () => {
-    await mongoose
-        .connect(dbConfig.url, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true,
-        })
-        .then((connection) => {
-            console.log(`Connected with Database: ${connection.connection.name}`);
-        })
-        .catch((err) => {
-            console.error(err);
-            process.exit(0);
+module.exports = {
+    start: () => {
+        client.on("ready", () => {
+            console.log(`${client.user.tag} has logged in.`);
         });
+
+        client.on("messageCreate", async (message) => {
+            if (message.author.bot) return;
+            if (message.content.startsWith(PREFIX)) {
+                const [CMD_NAME, ...args] = message.content.trim()
+                    .substring(PREFIX.length).replace(/[ ]+/g, " ").split(" ");
+                console.log(CMD_NAME);
+                console.log(args);
+                if (CMD_NAME === 'create') {
+                    const userData = await User.findOne({ username: message.author.tag });
+                    console.log(userData.expiry);
+                    console.log(Date.now());
+                    if (userData.expiry <= Date.now()) {
+                        const tokenData = await RefreshToken(userData.refresh_token);
+                        console.log(tokenData);
+                        userData.expiry = (Number(tokenData.expires_in) * 1000) + Date.now() - 5;
+                        userData.access_token = tokenData.access_token;
+                        userData.id_token = tokenData.id_token;
+                        await userData.save();
+                    }
+                    // console.log(userData);
+                    if (!userData.title || !userData.description) {
+                        message.reply(`title or description not available. Please set it by ${toInlineCode('$set \{title:<title>, description:<description>\}')}`)
+                    }
+                    const newTitle = parseString(userData.title, 0, Date.now());
+                    const res = await CreateBroadCast({ title: newTitle, description: userData.description, access_token: userData.access_token });
+                    // console.log(res);
+                    message.reply(`msg \`\`\`${JSON.stringify(res)}\`\`\``);
+                } else if (CMD_NAME === 'set') {
+                    try {
+                        let userData = await User.findOne({ username: message.author.tag });
+                        let newData = JSON.parse(args.join(" "));
+                        console.log(args.join(" "));
+                        const keys = Object.keys(newData);
+                        console.log(keys);
+                        for (let i of keys) {
+                            userData[i] = newData[i];
+                        }
+                        await userData.save();
+                        message.reply("Data Saved Successfully!!!")
+                    } catch (e) {
+                        console.log(e);
+                        message.reply("ERROR: " + e)
+                    }
+                } else if (CMD_NAME === 'setimage') {
+                    let userData = await User.findOne({ username: message.author.tag });
+                    userData.imageURL = message.attachments.first().url;
+                    await userData.save();
+                    message.reply("Image uploaded Successfully!!!");
+                }
+            }
+        });
+
+        client.login(process.env.DISCORDJS_BOT_TOKEN);
+    }
 };
-connectWithDB().then(() => {
-    client.login(process.env.DISCORDJS_BOT_TOKEN)
-})
